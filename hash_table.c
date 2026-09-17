8201f63e30af3b6d5e2f4c88cea855115260c01f
elfi8043 <elis.filen.8043@student.uu.se>
2026-09-17 16:45:11 +0200

    Added a draft for recursive destroy list

--- hash_table.c
+++ hash_table.c
@@ -29,89 +29,106 @@ ioopm_hash_table_t *ioopm_hash_table_create(void)
     return calloc(1, sizeof(ioopm_hash_table_t));
 }
 
+void destroy_linked_list(entry_t *prev, entry_t *next, entry_t *curr)
+{
+    if (next->next == NULL)
+    {
+        free(next);
+        return destroy_linked_list(curr, next->next, curr->next)
+    }
+
+    if ()
+}
+
 /// @brief Delete a hash table and free its memory
 /// @param ht a hash table to be deleted
 void ioopm_hash_table_destroy(ioopm_hash_table_t *ht)
-{
-    for (int i = 0; i < 17; i++) {
-        free(ht->buckets[i]);
+{ // TODO: make recursive?
+    // TODO: pekare på pekare på pekare på föregående
+    for (int i = 0; i < 17; i++)
+    {
+        entry_t *curr_list = ht->buckets[i];
+        if (curr_list == NULL)
+        {
+            continue;
+        }
     }
 }
 
 static size_t string_knr_hash(const char *str)
 {
-  size_t result = 0;
-  while (*str != '\0')
-  {
-    result = result * 31 + ((unsigned char) *str);
-    str++;
-  }
-  return result;
+    size_t result = 0;
+    while (*str != '\0')
+    {
+        result = result * 31 + ((unsigned char)*str);
+        str++;
+    }
+    return result;
 }
 
 void ioopm_hash_table_insert(ioopm_hash_table_t *ht, char *key, int value)
 {
-  // find bucket
-  size_t bucket = string_knr_hash(key) % 17;
+    // find bucket
+    size_t bucket = string_knr_hash(key) % 17;
 
-  // look for an entry with the key we want
-  entry_t *current = ht->buckets[bucket];
-  while (current != NULL && strcmp(current->key, key) != 0)
-  {
-    current = current->next;
-  }
-
-  // if the key exists, update the value, otherwise, add a new entry to the end of the list
-  if (current != NULL)
-  {
-    current->value = value;
-  }
-  else
-  {
-    // if the bucket is empty, we add a new first node
-    if (ht->buckets[bucket] == NULL)
+    // look for an entry with the key we want
+    entry_t *current = ht->buckets[bucket];
+    while (current != NULL && strcmp(current->key, key) != 0)
     {
-      ht->buckets[bucket] = malloc(sizeof(entry_t));
-      ht->buckets[bucket]->key = key;
-      ht->buckets[bucket]->value = value;
-      ht->buckets[bucket]->next = NULL;
+        current = current->next;
+    }
+
+    // if the key exists, update the value, otherwise, add a new entry to the end of the list
+    if (current != NULL)
+    {
+        current->value = value;
     }
     else
     {
-      // otherwise, we append a new node to the list
-      entry_t *last = ht->buckets[bucket];
-      while (last->next != NULL)
-      {
-        last = last->next;
-      }
-      last->next = malloc(sizeof(entry_t));
-      last->next->key = key;
-      last->next->value = value;
-      last->next->next = NULL;
+        // if the bucket is empty, we add a new first node
+        if (ht->buckets[bucket] == NULL)
+        {
+            ht->buckets[bucket] = malloc(sizeof(entry_t));
+            ht->buckets[bucket]->key = key;
+            ht->buckets[bucket]->value = value;
+            ht->buckets[bucket]->next = NULL;
+        }
+        else
+        {
+            // otherwise, we append a new node to the list
+            entry_t *last = ht->buckets[bucket];
+            while (last->next != NULL)
+            {
+                last = last->next;
+            }
+            last->next = malloc(sizeof(entry_t));
+            last->next->key = key;
+            last->next->value = value;
+            last->next->next = NULL;
+        }
     }
-  }
 }
 
 bool ioopm_hash_table_lookup(ioopm_hash_table_t *ht, char *key, int *result)
 {
-  // find bucket
-  size_t bucket = string_knr_hash(key) % 17;
+    // find bucket
+    size_t bucket = string_knr_hash(key) % 17;
 
-  // look for an entry with the key we want
-  entry_t *current = ht->buckets[bucket];
-  while (current != NULL && strcmp(current->key, key) != 0)
-  {
-    current = current->next;
-  }
+    // look for an entry with the key we want
+    entry_t *current = ht->buckets[bucket];
+    while (current != NULL && strcmp(current->key, key) != 0)
+    {
+        current = current->next;
+    }
 
-  // if the key exists, return the value, otherwise, indicate that the lookup failed
-  if (current != NULL)
-  {
-    *result = current->value;
-    return true;
-  }
-  else
-  {
-    return false;
-  }
+    // if the key exists, return the value, otherwise, indicate that the lookup failed
+    if (current != NULL)
+    {
+        *result = current->value;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
