7ba0354966c4aa3879dee843510ce0b832d5c5d6
alth5149 <alexander.thoresson.5149@student.uu.se>
2026-09-25 10:24:16 +0200

    Added a word struct

--- freq.c
+++ freq.c
@@ -8,8 +8,8 @@
 #include <assert.h>
 
 //
-//     If the word is not in the hash table, put it there, and give it the value 1
-//     If the word is in the hash table, Increment its current value by 1
+//     If the word is not in the hash table, put it there, and give it the frequency 1
+//     If the word is in the hash table, Increment its current frequency by 1
 //
 // Get all hash table keys and sort according to frequency.
 // For each key, in the sorted order
@@ -17,13 +17,26 @@
 //     Print the key and its frequency
 //
 // Tear down the hash table, free all resources, close all files
+
+typedef struct word word_t;
+
+struct word
+{
+    char *key;     // holds the key
+    int frequency;     // holds the frequency
+};
+
+//char *file_to_string(char *file_name) {
+
+//}
+
 int main(int argc, char *argv[])
 {
     // Create an empty hash table
     ioopm_hash_table_t *ht = ioopm_hash_table_create();
 
     // For each file argument,
-    for (int i = 1; i < 3; i++)
+    for (int i = 1; i < argc; i++)
     {
         char *filename = argv[i];
         puts("hello");
@@ -38,19 +51,19 @@ int main(int argc, char *argv[])
         {
             // printf("Retrieved line of length %zd:\n", nread);
             // fwrite(result, nread, 1, stdout);
-            char *word;
-            word = strtok(result, ",.!? :;\n");
-            while (word != NULL)
+            word_t word;
+            word.key = strtok(result, ",.!? :;\n");
+            while (word.key != NULL)
             {
-                if (ioopm_hash_table_has_key(ht, word)) {
-                    int val = 0;
-                    ioopm_hash_table_lookup(ht, word, &val);
-                    val += 1;
-                    ioopm_hash_table_insert(ht, word, val);
+                if (ioopm_hash_table_has_key(ht, word.key)) {
+                    word.frequency = 0;
+                    ioopm_hash_table_lookup(ht, word.key, &word.frequency);
+                    word.frequency += 1;
+                    ioopm_hash_table_insert(ht, word.key, word.frequency);
                 } else {
-                    ioopm_hash_table_insert(ht, word, 1);
+                    ioopm_hash_table_insert(ht, word.key, 1);
                 }
-                word = strtok(NULL, ",.!? :;\n");
+                word.key = strtok(NULL, ",.!? :;\n");
             }
 
             // free(result);
